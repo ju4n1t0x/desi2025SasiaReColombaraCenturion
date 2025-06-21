8e5e841cec03930cd38e17cb9eae7b30c76a1b6c
Juan Sasia <juanignaciosasia@gmail.com>
2025-06-21 01:01:06 -0300

    me falto guardar un cambio :)

--- src/main/java/tuti/desi/services/IAsistidoService.java
+++ src/main/java/tuti/desi/services/IAsistidoService.java
@@ -10,14 +10,19 @@ import tuti.desi.entidades.Familia;
 
 public interface IAsistidoService {
 
+	//método para traer todos los asistidos
 	public List<Asistido> getAll();
 	
+	//método para guardar un nuevo asistido
 	public void saveAsistido (Asistido asistido);
 	
+	//método para borrar un asistido
 	public void deleteAsistido (Long id);
 	
+	//método para buscar por id un asistido
 	public Asistido findAsistido (Long Id);
 	
+	//metodo para editar un asistido
 	public void editAsistido (Long idOriginal, Long nuevoId, Integer nuevoDni, String nuevoDomicilio, String nuevoNombre, String nuevoApellido, Date nuevaFechaNac, String nuevaOcupacion, Date nuevaFechaReg, Familia nuevaFamilia);
 }
 
