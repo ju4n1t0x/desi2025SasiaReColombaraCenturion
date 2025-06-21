99472ab879d43397995fc136dc9bbd851920a0b5
Juan Sasia <juanignaciosasia@gmail.com>
2025-06-21 19:26:34 -0300

    rama mia creada

--- src/main/java/tuti/desi/presentacion/asistido/AsistidoController.java
+++ src/main/java/tuti/desi/presentacion/asistido/AsistidoController.java
@@ -1,4 +1,4 @@
-package tuti.desi.presentacion;
+package tuti.desi.presentacion.asistido;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
