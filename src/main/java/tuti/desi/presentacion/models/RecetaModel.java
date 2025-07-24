09e7e5092628870a0e7f68d915162a37bcf8e557
ju4n1t0x <juanignaciosasia@gmail.com>
2025-07-24 18:19:07 -0300

    feat: Implementa lógica de stock y corrige errores de formulario

--- src/main/java/tuti/desi/presentacion/models/RecetaModel.java
+++ src/main/java/tuti/desi/presentacion/models/RecetaModel.java
@@ -10,9 +10,38 @@ public class RecetaModel {
 	private long id;
 	private String nombre;
 	private String descripcion;
-	
+
 	private List<ItemRecetaModel> itemReceta;
-	
+
 	private List<PreparacionModel> preparaciones;
-	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public List<ItemRecetaModel> getItemReceta() {
+		return itemReceta;
+	}
 }
+
+
